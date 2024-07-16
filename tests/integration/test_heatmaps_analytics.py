349e9d62b992985b8ec5a20a90502cef656d68c0
Mohammad Amin <dadgaramin96@gmail.com>
2024-07-16 08:38:13 +0330

    fix: now active users are being used! we had 5 active users rather than 1 user in rawmembers collection.

--- tests/integration/test_heatmaps_analytics.py
+++ tests/integration/test_heatmaps_analytics.py
@@ -39,7 +39,7 @@ class TestHeatmapsAnalytics(TestCase):
 
         self.mongo_client[platform_id]["rawmembers"].insert_one(
             {
-                "id": 9001,
+                "id": "9001",
                 "is_bot": False,
                 "left_at": None,
                 "joined_at": datetime(2023, 1, 1),
@@ -49,7 +49,7 @@ class TestHeatmapsAnalytics(TestCase):
 
         sample_raw_data = [
             {
-                "author_id": 9001,
+                "author_id": "9001",
                 "date": day + timedelta(hours=1),
                 "source_id": "10000",
                 "metadata": {"thread_id": "7000", "channel_id": "124"},
@@ -58,14 +58,14 @@ class TestHeatmapsAnalytics(TestCase):
                     {
                         "name": "reply",
                         "users_engaged_id": [
-                            9003,
+                            "9003",
                         ],
                         "type": "emitter",
                     },
                 ],
             },
             {
-                "author_id": 9001,
+                "author_id": "9001",
                 "date": day + timedelta(hours=1),
                 "source_id": "10005",
                 "metadata": {"thread_id": "7000", "channel_id": "124"},
@@ -74,57 +74,69 @@ class TestHeatmapsAnalytics(TestCase):
                     {
                         "name": "reaction",
                         "users_engaged_id": [
-                            9009,
+                            "9009",
                         ],
                         "type": "emitter",
                     }
                 ],
             },
             {
-                "author_id": 9001,
+                "author_id": "9001",
                 "date": day,
                 "source_id": "10001",
-                "metadata": {"thread_id": "7000", "channel_id": "124"},
+                "metadata": {
+                    "thread_id": "7000",
+                    "channel_id": "124",
+                    "bot_activity": False,
+                },
                 "actions": [{"name": "message", "type": "emitter"}],
                 "interactions": [
                     {
                         "name": "mention",
-                        "users_engaged_id": [9003, 9002],
+                        "users_engaged_id": ["9003", "9002"],
                         "type": "receiver",
                     }
                 ],
             },
             {
-                "author_id": 9001,
+                "author_id": "9001",
                 "date": day + timedelta(hours=2),
                 "source_id": "10003",
-                "metadata": {"thread_id": "7000", "channel_id": "124"},
+                "metadata": {
+                    "thread_id": "7000",
+                    "channel_id": "124",
+                    "bot_activity": False,
+                },
                 "actions": [{"name": "message", "type": "emitter"}],
                 "interactions": [
                     {
                         "name": "reply",
                         "users_engaged_id": [
-                            9003,
+                            "9003",
                         ],
                         "type": "receiver",
                     }
                 ],
             },
             {
-                "author_id": 9001,
+                "author_id": "9001",
                 "date": day + timedelta(hours=4),
                 "source_id": "10004",
-                "metadata": {"thread_id": None, "channel_id": "124"},
+                "metadata": {
+                    "thread_id": None,
+                    "channel_id": "124",
+                    "bot_activity": False,
+                },
                 "actions": [{"name": "message", "type": "emitter"}],
                 "interactions": [
                     {
                         "name": "mention",
-                        "users_engaged_id": [9003, 9002],
+                        "users_engaged_id": ["9003", "9002"],
                         "type": "emitter",
                     },
                     {
                         "name": "mention",
-                        "users_engaged_id": [9008, 9007],
+                        "users_engaged_id": ["9008", "9007"],
                         "type": "emitter",
                     },
                 ],
@@ -138,50 +150,51 @@ class TestHeatmapsAnalytics(TestCase):
 
         self.assertIsInstance(analytics, list)
 
-        # 3 iteration for heatmaps analytics
-        # (3 resources) * (1 rawmember) * (1 day)
-        self.assertEqual(len(analytics), 3)
+        # 5 iteration for heatmaps analytics
+        # (1 resources) * (5 rawmember) * (1 day)
+        self.assertEqual(len(analytics), 5)
 
-        for i in range(3):
-            # the second resource "124"
-            if i == 1:
-                self.assertEqual(sum(analytics[i]["thr_messages"]), 3)
-                self.assertEqual(sum(analytics[i]["lone_messages"]), 1)
-                self.assertEqual(sum(analytics[i]["replier"]), 1)
-                self.assertEqual(sum(analytics[i]["replied"]), 1)
-                # 4 people
-                self.assertEqual(sum(analytics[i]["mentioner"]), 4)
-                self.assertEqual(sum(analytics[i]["mentioned"]), 2)
-                self.assertEqual(sum(analytics[i]["reacter"]), 0)
-                self.assertEqual(sum(analytics[i]["reacted"]), 1)
+        for i in range(5):
+            if analytics[i]["user"] == "9001":
+                # the second resource "124"
+                if i == 4:
+                    self.assertEqual(sum(analytics[i]["thr_messages"]), 3)
+                    self.assertEqual(sum(analytics[i]["lone_messages"]), 1)
+                    self.assertEqual(sum(analytics[i]["replier"]), 1)
+                    self.assertEqual(sum(analytics[i]["replied"]), 1)
+                    # 4 people
+                    self.assertEqual(sum(analytics[i]["mentioner"]), 4)
+                    self.assertEqual(sum(analytics[i]["mentioned"]), 2)
+                    self.assertEqual(sum(analytics[i]["reacter"]), 0)
+                    self.assertEqual(sum(analytics[i]["reacted"]), 1)
 
-                self.assertEqual(
-                    analytics[i]["replied_per_acc"],
-                    [
-                        {
-                            "account": 9003,
-                            "count": 1,
-                        }
-                    ],
-                )
-                self.assertIn(
-                    {"account": 9003, "count": 1},
-                    analytics[i]["mentioner_per_acc"],
-                )
-                self.assertIn(
-                    {"account": 9002, "count": 1},
-                    analytics[i]["mentioner_per_acc"],
-                )
+                    self.assertEqual(
+                        analytics[i]["replied_per_acc"],
+                        [
+                            {
+                                "account": "9003",
+                                "count": 1,
+                            }
+                        ],
+                    )
+                    self.assertIn(
+                        {"account": "9003", "count": 1},
+                        analytics[i]["mentioner_per_acc"],
+                    )
+                    self.assertIn(
+                        {"account": "9002", "count": 1},
+                        analytics[i]["mentioner_per_acc"],
+                    )
 
-                self.assertEqual(
-                    analytics[i]["reacted_per_acc"],
-                    [
-                        {
-                            "account": 9009,
-                            "count": 1,
-                        }
-                    ],
-                )
+                    self.assertEqual(
+                        analytics[i]["reacted_per_acc"],
+                        [
+                            {
+                                "account": "9009",
+                                "count": 1,
+                            }
+                        ],
+                    )
             else:
                 self.assertEqual(sum(analytics[i]["thr_messages"]), 0)
                 self.assertEqual(sum(analytics[i]["lone_messages"]), 0)

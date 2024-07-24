afddb5ea03b1df8caabb0096df38d4fd3b3af5c3
Mohammad Amin <dadgaramin96@gmail.com>
2024-07-24 13:04:35 +0330

    feat: Added recompute mode!

--- tests/integration/test_closeness_centrality_with_mutul_ties.py
+++ tests/integration/test_closeness_centrality_with_mutul_ties.py
@@ -183,3 +183,99 @@ class TestClosenessCentralityWithMutualTies(TestCase):
                 self.assertAlmostEqual(row["closenessScore"], 2 / 3)
             else:
                 raise ValueError("Never should reach here!")
+
+    def test_three_vertices_two_dates_from_start_true(self):
+        # timestamps
+        today = 1689280200.0
+        yesterday = 1689193800.0
+        platform_id = "5151515151515"
+
+        user_label = self.graph_schema.user_label
+        platform_label = self.graph_schema.platform_label
+        interacted_with = self.graph_schema.interacted_with_rel
+        is_member = self.graph_schema.member_relation
+
+        # creating some nodes with data
+        self.neo4j_ops.gds.run_cypher(
+            f"""
+            CREATE (a:{user_label}) -[:{is_member}]->(g:{platform_label} {{id: '{platform_id}'}})
+            CREATE (b:{user_label}) -[:{is_member}]->(g)
+            CREATE (c:{user_label}) -[:{is_member}]->(g)
+            SET a.id = "a"
+            SET b.id = "b"
+            SET c.id = "c"
+            MERGE (a) -[r:{interacted_with} {{weight: 1, date: {today}}}]->(b)
+            MERGE (b) -[r2:{interacted_with} {{weight: 1, date: {today}}}]->(a)
+
+            MERGE (a) -[r3:{interacted_with} {{weight: 1, date: {yesterday}}}]->(b)
+            MERGE (b) -[r4:{interacted_with} {{weight: 2, date: {yesterday}}}]->(a)
+
+            MERGE (a) -[r5:{interacted_with} {{weight: 3, date: {today}}}]->(c)
+            MERGE (c) -[r6:{interacted_with} {{weight: 3, date: {today}}}]->(a)
+
+            MERGE (a) -[r7:{interacted_with} {{weight: 3, date: {yesterday}}}]->(c)
+            MERGE (c) -[r8:{interacted_with} {{weight: 3, date: {yesterday}}}]->(a)
+
+            MERGE (b) -[r9:{interacted_with} {{weight: 2, date: {today}}}]->(c)
+            MERGE (g)-[:HAVE_METRICS {{date: {today}}}]->(g)
+
+            // random scores
+            MERGE (a)-[:HAVE_METRICS {{platformId: '{platform_id}', date: {yesterday}, closenessCentrality: 0.1}}]->(a)
+            MERGE (b)-[:HAVE_METRICS {{platformId: '{platform_id}', date: {yesterday}, closenessCentrality: 0.1}}]->(b)
+            MERGE (c)-[:HAVE_METRICS {{platformId: '{platform_id}', date: {yesterday}, closenessCentrality: 0.1}}]->(c)
+
+            SET r.platformId = '{platform_id}'
+            SET r2.platformId = '{platform_id}'
+            SET r3.platformId = '{platform_id}'
+            SET r4.platformId = '{platform_id}'
+            SET r5.platformId = '{platform_id}'
+            SET r6.platformId = '{platform_id}'
+            SET r7.platformId = '{platform_id}'
+            SET r8.platformId = '{platform_id}'
+            SET r9.platformId = '{platform_id}'
+            """
+        )
+
+        centrality = ClosenessCentrality(platform_id, self.graph_schema)
+        centrality.compute(from_start=True)
+
+        yesterday_results = self.neo4j_ops.gds.run_cypher(
+            f"""
+            MATCH (user:{self.graph_schema.user_label})-[r:HAVE_METRICS {{platformId: '{platform_id}', date: {yesterday}}}]->(user)
+            RETURN user.id as userid, r.date as date, r.closenessCentrality as closenessScore
+            """
+        )
+        self.assertEqual(len(yesterday_results), 3)
+
+        # the yesterday scores should be recomputed
+        for _, row in yesterday_results.iterrows():
+            self.assertEqual(row["date"], yesterday)
+
+            if row["userid"] == "a":
+                self.assertEqual(row["closenessScore"], 1)
+            elif row["userid"] == "b":
+                self.assertAlmostEqual(row["closenessScore"], 2 / 3)
+            elif row["userid"] == "c":
+                self.assertAlmostEqual(row["closenessScore"], 2 / 3)
+            else:
+                raise ValueError("Never should reach here!")
+
+        today_results = self.neo4j_ops.gds.run_cypher(
+            f"""
+            MATCH (user:{self.graph_schema.user_label})-[r:HAVE_METRICS {{platformId: '{platform_id}', date: {today}}}]->(user)
+            RETURN user.id as userid, r.date as date, r.closenessCentrality as closenessScore
+            """
+        )
+
+        self.assertEqual(len(today_results), 3)
+        for _, row in today_results.iterrows():
+            self.assertEqual(row["date"], today)
+
+            if row["userid"] == "a":
+                self.assertEqual(row["closenessScore"], 1)
+            elif row["userid"] == "b":
+                self.assertAlmostEqual(row["closenessScore"], 2 / 3)
+            elif row["userid"] == "c":
+                self.assertAlmostEqual(row["closenessScore"], 2 / 3)
+            else:
+                raise ValueError("Never should reach here!")
